18dfa06dcf4b0c5014ac16f7eef6b4d0f55ce7b7
Nate Crawford <nathanwcrawford22@gmail.com>
2022-02-24 15:07:17 -0500

    Final commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Instagrames.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Instagrames.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly InstaDb db;
+
+        public HomeController(ILogger<HomeController> logger, InstaDb fromstartupcs)
+        {
+            _logger = logger;
+            db = fromstartupcs;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View(db.Users.Include(u => u.Posts).ToList());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
